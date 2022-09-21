b64f46b80d32b9f0e3b17e340760a30e5d47ee6f
James Wurbel <jameswurbel@gmail.com>
2022-09-21 16:15:14 -0700

    Adding packer-manifest-parser dir

---
+++ packer-manifest-parser/main.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+import json
+
+json_file = "/home/james/Packer_Templates/ubuntu-aws/packer-manifest.json"
+
+
+def ami(file):
+    """Parses packer-manifest json file and returns region and latest build ami"""
+    with open(file, "r") as file:
+        f = file.read()
+        manifest = json.loads(f)
+        for k, v in manifest.items():
+            if k == "builds":
+                last_build = v[-1]
+                image = last_build.get("artifact_id").split(":")
+                print(f"Region: {image[0]} \nImage: {image[1]}")
+
+
+if __name__ == '__main__':
+    ami(json_file)
